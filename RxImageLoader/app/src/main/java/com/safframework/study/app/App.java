79a0f540f74ab0ca822679d79d8028fe5edd21ca
fengzhizi715 <fengzhizi715@126.com>
2017-06-23 00:19:18 +0800

    add App for RxImageLoader demo

---
+++ RxImageLoader/app/src/main/java/com/safframework/study/app/App.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package com.safframework.study.app;
+
+import android.annotation.TargetApi;
+import android.app.Application;
+import android.content.ComponentCallbacks2;
+
+import com.safframework.study.rximageloader.RxImageLoader;
+import com.safframework.study.rximageloader.Utils;
+
+/**
+ *
+ * 
+ * @author Tony Shen
+ * 
+ */
+public class App extends Application {
+
+	public RxImageLoader imageLoader;
+	private static App instance;
+
+	/**
+	 * @see Application#onCreate()
+	 */
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		init();
+	}
+
+	public void init() {
+		instance = this;
+
+		imageLoader = new RxImageLoader();
+		imageLoader.init(instance);
+	}
+
+	public static App getInstance() {
+		return instance;
+	}
+
+	@Override
+	public void onLowMemory() {
+		super.onLowMemory();
+		imageLoader.clearMemCache();
+	}
+	
+	@Override
+	@TargetApi(14)
+	public void onTrimMemory(int level) {
+		if (Utils.isICSOrHigher()) {
+			super.onTrimMemory(level);
+			
+			if (level >= ComponentCallbacks2.TRIM_MEMORY_RUNNING_LOW) {
+				imageLoader.clearMemCache();
+			}
+		}
+	}
+}
