d71593816e8175471ce15f13052a28a1f4d33a21
fengzhizi715 <fengzhizi715@126.com>
2017-09-11 10:00:51 +0800

    add TestHttpClientWithObservable

---
+++ HttpClient/httpClient/src/main/java/com/safframework/study/httpclient/TestHttpClientWithObservable.java
@@ -0,0 +1,66 @@
+package com.safframework.study.httpclient;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.util.function.Function;
+
+/**
+ * Created by tony on 2017/9/11.
+ */
+public class TestHttpClientWithObservable {
+
+    public static void main(String[] args) {
+
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
+
+                String url = "http://www.163.com";
+                e.onNext(url);
+            }
+        }).map(new io.reactivex.functions.Function<String, CloseableHttpResponse>() {
+
+            @Override
+            public CloseableHttpResponse apply(@NonNull String url) throws Exception {
+
+                CloseableHttpClient client = HttpClients.createDefault();
+
+                HttpGet get = new HttpGet(url);
+
+                return client.execute(get);
+            }
+        }).subscribe(new Consumer<CloseableHttpResponse>() {
+            @Override
+            public void accept(CloseableHttpResponse response) throws Exception {
+
+                // 服务器返回码
+                int status_code = response.getStatusLine().getStatusCode();
+                System.out.println("status_code = " + status_code);
+
+                HttpEntity entity = response.getEntity();
+
+                // 服务器返回内容
+                String respStr = null;
+
+                if (entity != null) {
+                    respStr = EntityUtils.toString(entity, "UTF-8");
+                }
+
+                // 释放资源
+                EntityUtils.consume(entity);
+
+                System.out.println("respStr = " + respStr);
+            }
+        });
+    }
+}
