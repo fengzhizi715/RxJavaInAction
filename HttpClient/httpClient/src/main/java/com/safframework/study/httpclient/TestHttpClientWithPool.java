161388b44f7c49b869adb2351fcbf7bfdab8d1d2
fengzhizi715 <fengzhizi715@126.com>
2017-09-11 13:55:57 +0800

    优化代码

--- HttpClient/httpClient/src/main/java/com/safframework/study/httpclient/TestHttpClientWithPool.java
+++ HttpClient/httpClient/src/main/java/com/safframework/study/httpclient/TestHttpClientWithPool.java
@@ -2,23 +2,17 @@ package com.safframework.study.httpclient;
 
 import org.apache.http.*;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.protocol.HttpContext;
+
 import org.apache.http.util.EntityUtils;
 
-import javax.net.ssl.SSLException;
-import javax.net.ssl.SSLHandshakeException;
 import java.io.IOException;
-import java.io.InterruptedIOException;
-import java.net.UnknownHostException;
+
 
 /**
  * Created by tony on 2017/9/11.
@@ -55,51 +49,12 @@ public class TestHttpClientWithPool {
                 .setSocketTimeout(timeOut)
                 .build();
 
-        /**
-         * 测出超时重试机制为了防止超时不生效而设置
-         *     如果直接放回false,不重试
-         *  这里会根据情况进行判断是否重试
-         */
-        HttpRequestRetryHandler retry = new HttpRequestRetryHandler() {
-            @Override
-            public boolean retryRequest(IOException exception, int executionCount, HttpContext context) {
-                if (executionCount >= 3) {// 如果已经重试了3次，就放弃
-                    return false;
-                }
-                if (exception instanceof NoHttpResponseException) {// 如果服务器丢掉了连接，那么就重试
-                    return true;
-                }
-                if (exception instanceof SSLHandshakeException) {// 不要重试SSL握手异常
-                    return false;
-                }
-                if (exception instanceof InterruptedIOException) {// 超时
-                    return true;
-                }
-                if (exception instanceof UnknownHostException) {// 目标服务器不可达
-                    return false;
-                }
-                if (exception instanceof ConnectTimeoutException) {// 连接被拒绝
-                    return false;
-                }
-                if (exception instanceof SSLException) {// ssl握手异常
-                    return false;
-                }
-                HttpClientContext clientContext = HttpClientContext.adapt(context);
-                HttpRequest request = clientContext.getRequest();
-                // 如果请求是幂等的，就再次尝试
-                if (!(request instanceof HttpEntityEnclosingRequest)) {
-                    return true;
-                }
-                return false;
-            }
-        };
-
         // 创建httpClient
         return HttpClients.custom()
                 // 把请求相关的超时信息设置到连接客户端
                 .setDefaultRequestConfig(requestConfig)
                 // 把请求重试设置到连接客户端
-                .setRetryHandler(retry)
+                .setRetryHandler(new RetryHandler())
                 // 配置连接池管理对象
                 .setConnectionManager(connManager)
                 .build();
@@ -113,7 +68,7 @@ public class TestHttpClientWithPool {
      * @return
      */
     public static String httpGet(String url, int timeOut) {
-        String msg = "-1";
+        String msg = null;
 
         // 获取客户端连接对象
         CloseableHttpClient httpClient = getHttpClient(timeOut);
