ea71b11a3bc0d117c707da7dda95c495e295a473
fengzhizi715 <fengzhizi715@126.com>
2017-09-16 17:26:26 +0800

    add TestFuture

---
+++ LearnJava8/java8/src/main/java/com/safframework/study/java8/TestFuture.java
@@ -0,0 +1,38 @@
+package com.safframework.study.java8;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Created by tony on 2017/9/16.
+ */
+public class TestFuture {
+
+    public static void main(String[] args) {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        Future<String> future = executor.submit(() -> { //Lambda 是一个 callable， 提交后便立即执行，这里返回的是 FutureTask 实例
+            System.out.println("running task");
+            return "return task";
+        });
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+
+        System.out.println("do something else");  //前面的的 Callable 在其他线程中运行着，可以做一些其他的事情
+
+        try {
+            System.out.println(future.get());  //等待 future 的执行结果，执行完毕之后打印出来
+        } catch (InterruptedException e) {
+        } catch (ExecutionException e) {
+
+        } finally {
+            executor.shutdown();
+        }
+    }
+}
