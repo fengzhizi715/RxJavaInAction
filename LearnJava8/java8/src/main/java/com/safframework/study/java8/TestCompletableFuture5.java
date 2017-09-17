164ae8fed4e7b0736edddb309792eabd5d11dcb4
fengzhizi715 <fengzhizi715@126.com>
2017-09-17 21:56:06 +0800

    add TestCompletableFuture5

---
+++ LearnJava8/java8/src/main/java/com/safframework/study/java8/TestCompletableFuture5.java
@@ -0,0 +1,24 @@
+package com.safframework.study.java8;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by tony on 2017/9/16.
+ */
+public class TestCompletableFuture5 {
+
+    public static void main(String[] args) {
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> "Hello");
+
+        future.completeExceptionally(new Exception());
+
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
