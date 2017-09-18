b63ceb30e9ba2b5f70b1be91d4806a2dfe71aa86
fengzhizi715 <fengzhizi715@126.com>
2017-09-18 17:31:23 +0800

    update rxbinding demo

--- RxBinding/app/src/main/java/com/safframework/study/rxbinding/activity/TestRecyclerViewActivity.java
+++ RxBinding/app/src/main/java/com/safframework/study/rxbinding/activity/TestRecyclerViewActivity.java
@@ -4,14 +4,19 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import com.jakewharton.rxbinding2.support.v7.widget.RxRecyclerView;
 import com.safframework.injectview.annotations.InjectView;
+import com.safframework.log.L;
 import com.safframework.study.rxbinding.R;
 import com.safframework.study.rxbinding.adapter.MyRecyclerAdapter;
 import com.safframework.study.rxbinding.app.BaseActivity;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.functions.Consumer;
 
 /**
  * Created by tony on 2017/9/18.
@@ -39,9 +44,20 @@ public class TestRecyclerViewActivity extends BaseActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(OrientationHelper.VERTICAL);
         recyclerview.setLayoutManager(linearLayoutManager);
+
+        RxRecyclerView
+                .scrollStateChanges(recyclerview)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer scrollState) throws Exception {
+
+                        L.i("scrollState = " + scrollState);
+                    }
+                });
     }
 
     private void initData() {
+
         mDatas = new ArrayList<>();
         for ( int i=0; i < 40; i++) {
             mDatas.add( "item "+i);
--- RxBinding/app/src/main/java/com/safframework/study/rxbinding/adapter/MyRecyclerAdapter.java
+++ RxBinding/app/src/main/java/com/safframework/study/rxbinding/adapter/MyRecyclerAdapter.java
@@ -6,53 +6,74 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.jakewharton.rxbinding2.view.RxView;
 import com.safframework.study.rxbinding.R;
+import com.safframework.tony.common.utils.Preconditions;
 
 import java.util.List;
+
+import io.reactivex.functions.Consumer;
 
 /**
  * Created by tony on 2017/9/18.
  */
 
-public class MyRecyclerAdapter  extends RecyclerView.Adapter<MyRecyclerAdapter.ViewHolder> {
+public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.ViewHolder> {
 
     private List<String> mDatas;
     private Context mContext;
     private LayoutInflater inflater;
 
-    public MyRecyclerAdapter(Context context, List<String> datas){
-        this. mContext=context;
-        this. mDatas=datas;
-        inflater=LayoutInflater. from(mContext);
+    public MyRecyclerAdapter(Context context, List<String> datas) {
+        this.mContext = context;
+        this.mDatas = datas;
+        inflater = LayoutInflater.from(mContext);
     }
 
     @Override
     public int getItemCount() {
 
-        return mDatas.size();
+        return Preconditions.isNotBlank(mDatas) ? mDatas.size() : 0;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
 
-        holder.tv.setText( mDatas.get(position));
+        final String item = mDatas.get(position);
+
+        if (Preconditions.isNotBlank(item)) {
+
+            holder.text.setText(item);
+
+            RxView.clicks(holder.itemView)
+                    .subscribe(new Consumer<Object>() {
+
+                        @Override
+                        public void accept(Object o) throws Exception {
+
+                            Toast.makeText(mContext,item,Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View view = inflater.inflate(R.layout.item_my_recyclerview,parent, false);
+        View view = inflater.inflate(R.layout.item_my_recyclerview, parent, false);
         return new ViewHolder(view);
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView tv;
+        TextView text;
 
         public ViewHolder(View view) {
             super(view);
-            tv=(TextView) view.findViewById(R.id.textview);
+            text = (TextView) view.findViewById(R.id.textview);
         }
 
     }
