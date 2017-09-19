146916af3075130b29a854df8cb3411c99568f54
fengzhizi715 <fengzhizi715@126.com>
2017-09-19 22:51:55 +0800

    update CompletableFuture demo

---
+++ LearnJava8/java8/src/main/java/com/safframework/study/java8/TestCompletableFuture8.java
@@ -0,0 +1,24 @@
+package com.safframework.study.java8;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by tony on 2017/9/19.
+ */
+public class TestCompletableFuture8 {
+
+    public static void main(String[] args) {
+
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> "Hello")
+                .thenCompose(s -> CompletableFuture.supplyAsync(() -> s + " World"));
+
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
