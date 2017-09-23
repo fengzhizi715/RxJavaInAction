2b4a418394383d955aaaddbbed11f1ebc517c066
fengzhizi715 <fengzhizi715@126.com>
2017-09-23 20:08:49 +0800

    update conpletablefuture demo

---
+++ LearnJava8/java8/src/main/java/com/safframework/study/java8/TestCompletableFuture19.java
@@ -0,0 +1,26 @@
+package com.safframework.study.java8;
+
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Function;
+
+/**
+ * Created by tony on 2017/9/21.
+ */
+public class TestCompletableFuture19 {
+
+    public static void main(String[] args) {
+
+        CompletableFuture.supplyAsync(() -> "hello world")
+                .thenApply(s -> {
+                    s = null;
+                    int length = s.length();
+                    return length;
+                }).thenAccept(i -> System.out.println(i))
+                .exceptionally(t -> {
+                    System.out.println("Unexpected error:" + t);
+                    return null;
+                });
+    }
+}
