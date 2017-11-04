b570575cdb5bcd34f6498214265ebd44e2e9a149
fengzhizi715 <fengzhizi715@126.com>
2017-11-04 23:33:10 +0800

    update StudyOperator

--- StudyOperator/app/src/main/java/com/safframework/study/operator/app/BaseActivity.java
+++ StudyOperator/app/src/main/java/com/safframework/study/operator/app/BaseActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import com.safframework.injectview.Injector;
 
 /**
- * Created by tony on 2017/11/4.
+ * Created by Tony Shen on 2017/3/12.
  */
 
 public class BaseActivity extends AppCompatActivity {
