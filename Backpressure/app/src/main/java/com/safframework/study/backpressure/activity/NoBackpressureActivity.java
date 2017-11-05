3c6a94432915a738dad3c2dba6c3e3cb3de95a2d
fengzhizi715 <fengzhizi715@126.com>
2017-11-05 23:03:20 +0800

    update Backpressure demo

--- Backpressure/app/src/main/java/com/safframework/study/backpressure/activity/NoBackpressureActivity.java
+++ Backpressure/app/src/main/java/com/safframework/study/backpressure/activity/NoBackpressureActivity.java
@@ -1,10 +1,36 @@
 package com.safframework.study.backpressure.activity;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import com.safframework.study.backpressure.app.BaseActivity;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
 
 /**
  * Created by tony on 2017/11/5.
  */
 
 public class NoBackpressureActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                for (int i = 0;; i++) {   //无限循环发事件
+                    subscriber.onNext(i);
+                }
+            }
+        }).subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                Log.d("NoBackpressureActivity", String.valueOf(integer));
+            }
+        });
+    }
 }
