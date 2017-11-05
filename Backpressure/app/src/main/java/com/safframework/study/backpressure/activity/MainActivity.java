7544a6d0d3ab719b00d89627f0104436b6a01cec
fengzhizi715 <fengzhizi715@126.com>
2017-11-05 22:55:09 +0800

    update Backpressure demo

---
+++ Backpressure/app/src/main/java/com/safframework/study/backpressure/activity/MainActivity.java
@@ -0,0 +1,39 @@
+package com.safframework.study.backpressure.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.safframework.injectview.annotations.InjectView;
+import com.safframework.study.backpressure.R;
+import com.safframework.study.backpressure.app.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @InjectView(R.id.text1)
+    TextView text1;
+
+    @InjectView(R.id.text2)
+    TextView text2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        text2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
