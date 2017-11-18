7192096f9020f762f6a0c1710890b40bf011f2d8
fengzhizi715 <fengzhizi715@126.com>
2017-11-19 00:33:13 +0800

    add Tasks project

---
+++ Tasks/src/main/java/com/safframework/study/task/Application.java
@@ -0,0 +1,15 @@
+package com.safframework.study.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by tony on 2017/11/19.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
