fecea7df76d3499329cae463ffa9b543e2b1ba24
fengzhizi715 <fengzhizi715@126.com>
2017-12-15 23:35:44 +0800

    add CreateActivity

---
+++ StudyOperator/app/src/main/java/com/safframework/study/operator/activity/create/CreateActivity.java
@@ -0,0 +1,11 @@
+package com.safframework.study.operator.activity.create;
+
+import com.safframework.study.operator.app.BaseActivity;
+
+/**
+ * Created by tony on 2017/12/15.
+ */
+
+public class CreateActivity extends BaseActivity {
+
+}
