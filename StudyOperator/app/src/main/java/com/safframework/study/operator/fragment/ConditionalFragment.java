9a851e91682a15f158f7a318f6413c9aadd1c783
fengzhizi715 <fengzhizi715@126.com>
2017-12-16 22:30:51 +0800

    update ConditionalFragment

--- StudyOperator/app/src/main/java/com/safframework/study/operator/fragment/ConditionalFragment.java
+++ StudyOperator/app/src/main/java/com/safframework/study/operator/fragment/ConditionalFragment.java
@@ -1,5 +1,12 @@
 package com.safframework.study.operator.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.safframework.injectview.Injector;
+import com.safframework.study.operator.R;
 import com.safframework.study.operator.app.BaseFragment;
 
 /**
@@ -7,4 +14,17 @@ import com.safframework.study.operator.app.BaseFragment;
  */
 
 public class ConditionalFragment extends BaseFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_conditional, container, false);
+        Injector.injectInto(this, v);
+
+        initViews();
+
+        return v;
+    }
+
+    private void initViews() {
+    }
 }
