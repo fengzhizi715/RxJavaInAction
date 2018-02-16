36fc8359e1f2f458e8781d9d78e52d02f232dec6
fengzhizi715 <fengzhizi715@126.com>
2018-02-16 22:29:52 +0800

    update Rxbiding demo

--- RxBinding/app2/src/main/java/com/safframework/study/rxpermissons/activity/MainActivity.java
+++ RxBinding/app2/src/main/java/com/safframework/study/rxpermissons/activity/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import com.jakewharton.rxbinding2.view.RxView;
 import com.safframework.injectview.annotations.InjectView;
+import com.safframework.log.L;
 import com.safframework.study.rxpermissons.R;
 import com.safframework.study.rxpermissons.app.BaseActivity;
 import com.tbruyelle.rxpermissions2.RxPermissions;
@@ -25,6 +26,9 @@ public class MainActivity extends BaseActivity {
     @InjectView(R.id.text2)
     TextView text2;
 
+    @InjectView(R.id.text3)
+    TextView text3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,7 @@ public class MainActivity extends BaseActivity {
         initViews();
     }
 
+    @SuppressLint("MissingPermission")
     private void initViews() {
 
         final RxPermissions rxPermissions = new RxPermissions(this);
@@ -56,19 +61,47 @@ public class MainActivity extends BaseActivity {
                                             startActivity(intent);
                                         } else {
 
-
+                                            L.i("授权失败");
                                         }
                                     }
                                 });
                     }
                 });
 
-        RxView.longClicks(text2)
-                .subscribe(new Consumer<Object>() {
-                    @Override
-                    public void accept(@NonNull Object o) throws Exception {
+        RxView.clicks(text2)
+                .compose(rxPermissions.ensure(Manifest.permission.CALL_PHONE))
+                .subscribe(new Consumer<Boolean>() {
 
-                        Toast.makeText(MainActivity.this,"演示长点击事件",Toast.LENGTH_SHORT).show();
+                    @Override
+                    public void accept(@NonNull Boolean granted) throws Exception {
+
+                        if (granted) {
+
+                            Intent intent = new Intent(Intent.ACTION_CALL);
+                            intent.setData(Uri.parse("tel:" + "10000"));
+                            startActivity(intent);
+                        } else {
+
+                            L.i("授权失败");
+                        }
+                    }
+                });
+
+        RxView.clicks(text3)
+                .compose(rxPermissions.ensure(Manifest.permission.CAMERA,
+                        Manifest.permission.READ_PHONE_STATE))
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean granted) throws Exception {
+
+                        if (granted) {
+
+                            Toast.makeText(MainActivity.this,"打开相机成功",Toast.LENGTH_SHORT).show();
+                        } else {
+
+                            L.i("授权失败");
+                            Toast.makeText(MainActivity.this,"打开相机失败",Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
     }
