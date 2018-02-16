dc9146e7153a8e122a31b040d08643f5ca8bec76
fengzhizi715 <fengzhizi715@126.com>
2018-02-16 21:56:17 +0800

    update RxBingding demo

--- RxBinding/app2/src/main/java/com/safframework/study/rxpermissons/activity/MainActivity.java
+++ RxBinding/app2/src/main/java/com/safframework/study/rxpermissons/activity/MainActivity.java
@@ -1,54 +1,51 @@
 package com.safframework.study.rxpermissons.activity;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jakewharton.rxbinding2.view.RxView;
+import com.safframework.injectview.annotations.InjectView;
+import com.safframework.study.rxpermissons.R;
 import com.safframework.study.rxpermissons.app.BaseActivity;
+
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends BaseActivity {
 
-//    @InjectView(R.id.text1)
-//    TextView text1;
-//
-//    @InjectView(R.id.text2)
-//    TextView text2;
-//
-//    @InjectView(R.id.text3)
-//    TextView text3;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        initViews();
-//    }
-//
-//    private void initViews() {
-//
-//        RxView.clicks(text1)
-//                .subscribe(new Consumer<Object>() {
-//                    @Override
-//                    public void accept(@NonNull Object o) throws Exception {
-//
-//                        Toast.makeText(MainActivity.this,"演示点击事件",Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//
-//        RxView.longClicks(text2)
-//                .subscribe(new Consumer<Object>() {
-//                    @Override
-//                    public void accept(@NonNull Object o) throws Exception {
-//
-//                        Toast.makeText(MainActivity.this,"演示长点击事件",Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//
-//        RxView.clicks(text3)
-//                .compose(RxUtils.useRxViewTransformer(MainActivity.this))
-//                .subscribe(new Consumer<Object>() {
-//                    @Override
-//                    public void accept(@NonNull Object o) throws Exception {
-//
-//                        Toast.makeText(MainActivity.this,"防止重复点击",Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//    }
+    @InjectView(R.id.text1)
+    TextView text1;
+
+    @InjectView(R.id.text2)
+    TextView text2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+
+        RxView.clicks(text1)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(@NonNull Object o) throws Exception {
+
+                        Toast.makeText(MainActivity.this,"演示点击事件",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        RxView.longClicks(text2)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(@NonNull Object o) throws Exception {
+
+                        Toast.makeText(MainActivity.this,"演示长点击事件",Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
